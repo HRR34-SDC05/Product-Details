3c8f5e43f15128bb86457c923f1cdad7292ffbf6
Akatsuki <user@users-MacBook-Pro.local>
2018-11-30 11:03:52 -0500

    modified to migrate to AWS

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('newrelic');
+// require('newrelic');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,29 +8,27 @@ const config = require('../database/postgres/config.json');
 
 const app = express();
 
-const host = config.host;
-const user = config.user;
-const pw = config.pw;
-const db = config.db;
-const port = config.port;
+const environ = 'development';
+
+const host = config[environ].host;
+const user = config[environ].user;
+const pw = config[environ].pw;
+const db = config[environ].db;
+const port = config[environ].port;
 const conString = `postgres://${user}:${pw}@${host}:${port}/${db}`;
 
 const client = new Client({
   connectionString: conString,
-  // database: 'gs_details'
 });
 client.connect();
-// const Products = require('../database/postgres/index.js');
-client.query('SELECT * FROM details WHERE id = 68')
- .then((resp) => { console.log(resp.rows[0].images.split(',')); });
 
-const PORT = 8081;
+const PORT = 3030;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept',
   );
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
@@ -39,13 +37,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/../client/dist`, { maxAge: '365d' }));
 
-app.get('/', (req, res) => {
+app.get('/product/:id', (req, res) => {
   console.log('Made it into the very first load');
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  const file = path.join(`${__dirname}/../client/dist/index.html`);
+  res.sendFile(file);
 });
 
-app.get('/data', (req, res) => {
-  const id = Math.floor(Math.random() * 100000);
+app.get('/product/data/:id', (req, res) => {
+  const id = req.params.id || Math.floor(Math.random() * 100000);
   console.log('id in initial GET request --->', id);
   client.query(`SELECT * FROM details WHERE id = ${id}`)
     .then((resp) => {
@@ -53,7 +52,7 @@ app.get('/data', (req, res) => {
       resp.rows[0].colors = resp.rows[0].colors.split(',');
       resp.rows[0].rating = Number(resp.rows[0].rating);
       resp.rows[0].price = Number(resp.rows[0].price);
-      res.send(resp.rows[0]);
+      res.status(200).send(resp.rows[0]);
     })
     .catch(() => res.status(500).send('Could not receive GET request'));
   // Products.findById(id).exec((err, result) => {
@@ -68,7 +67,7 @@ app.get('/data', (req, res) => {
 // below are additional methods added for sake of stress testing
 
 app.post("/product/:id", (req, res) => {
-  res.status(200).send("post request completed");
+  res.status(201).send("post request completed");
 });
 
 app.delete("/product/:id", (req, res) => {
